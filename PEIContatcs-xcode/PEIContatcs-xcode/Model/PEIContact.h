a8ad151d1120bc2d3dfb6fe2a3cd53f03615c0d1
AustinPotts <pottsevolvements@gmail.com>
2020-03-25 20:24:50 -0400

    Configured Contact Model

---
+++ PEIContatcs-xcode/PEIContatcs-xcode/Model/PEIContact.h
@@ -0,0 +1,25 @@
+//
+//  PEIContact.h
+//  PEIContatcs-xcode
+//
+//  Created by Austin Potts on 3/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEIContact : NSObject
+
+@property (nonatomic, readonly) NSString *name;
+@property (nonatomic, readonly) NSString *email;
+@property (nonatomic, readonly) int *number;
+
+- (instancetype)initWithName:(NSString *)name
+                       email:(NSString *)email
+                      number:(int *)number;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PEIContatcs-xcode/PEIContatcs-xcode/Model/PEIContact.m
@@ -0,0 +1,29 @@
+//
+//  PEIContact.m
+//  PEIContatcs-xcode
+//
+//  Created by Austin Potts on 3/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "PEIContact.h"
+
+@implementation PEIContact
+
+- (instancetype)initWithName:(NSString *)name
+ email:(NSString *)email
+                      number:(int *)number {
+    
+    if (self = [super init]) {
+        _name = name;
+        _email = email;
+        _number = number;
+    }
+    return self;
+    
+}
+
+
+
+
+@end
